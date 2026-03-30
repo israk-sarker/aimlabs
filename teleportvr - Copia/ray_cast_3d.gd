d8c23e4c8823995358218e1062c420925c6f3ac6
Sarker Israk <israk.sarker@cz.lan>
2026-03-30 16:11:43 +0200

    v2

--- teleportvr - Copia/ray_cast_3d.gd
+++ teleportvr - Copia/ray_cast_3d.gd
@@ -1,19 +1,36 @@
 extends RayCast3D
 
-func gun_fired() -> void:
-	# Forza il raycast ad aggiornarsi immediatamente questo frame
+# Il TargetDot NON deve essere figlio della pistola.
+# Deve essere un nodo MeshInstance3D nella scena ROOT (es. figlio di World/Stanza).
+# Trascina quel nodo in questo campo dall'Inspector.
+@export var target_dot: MeshInstance3D
+
+@export var max_range: float = 20.0
+
+func _ready() -> void:
+	enabled = true
+
+func _process(_delta: float) -> void:
+	if target_dot == null:
+		return
+
 	force_raycast_update()
-	
-	# Controlla se ha colpito qualcosa
-	if not is_colliding():
-		return
-	
+
+	if is_colliding():
+		var hit_point: Vector3 = get_collision_point()
+		var hit_normal: Vector3 = get_collision_normal()
+
+		# Posiziona il dot nel world space, sul punto di impatto
+		target_dot.global_position = hit_point + hit_normal * 0.005
+		target_dot.visible = true
+	else:
+		# Nessuna superficie colpita: proietta il dot a max_range metri avanti
+		target_dot.global_position = global_position + (-global_transform.basis.z * max_range)
+		target_dot.visible = true
+
+func gun_fired() -> void:
+	force_raycast_update()
 	var hit_target = get_collider()
-	
-	# Sicurezza extra: verifica che il nodo esista ancora
-	if not is_instance_valid(hit_target):
-		return
-	
-	# Controlla il gruppo e chiama damage
-	if hit_target.is_in_group("target"):
-		hit_target.damage(1.0)
+	if hit_target != null:
+		if hit_target.is_in_group("target"):
+			hit_target.damage(1.0)
